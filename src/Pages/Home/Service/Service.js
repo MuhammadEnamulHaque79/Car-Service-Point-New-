d73c59a5aad8cb71f532b8cb64e5a9e6ad4bc465
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2022-10-07 12:34:01 +0600

    done responsive

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Service.css';
 
 const Service = ({service}) => {
-    const{img,name,price}=service;
+    const{img,name,price,description}=service;
     return (
         <div className='service-container'>
             <img src={img} alt="" />
-            <h2>Service:{name}</h2>
-            <h3>${price}</h3>
+            <h3>{name}</h3>
+            <h4>Price: ${price}</h4>
+            <p  style={{textAlign:'justify'}}><small>{description}</small></p>
+            <button>Book :{name}</button>
+            
         </div>
     );
 };
